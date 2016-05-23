c2c55121c0740bd4753edfc6675a955a39520fdb
gongqingkui WinTabletPc <gongqingkui@126.com.winpc>
2016-05-23 17:21:32 +0800

    activer function

--- code.py
+++ code.py
@@ -1,102 +1,69 @@
 # encoding: utf-8
-import web,os,sqlite3,time
+import web
+import os
+import sqlite3
+import time
 from web import form
-#templete floder
+activer = activer()
+# templete floder
 render = web.template.render('templates/')
-#url tables
+# url tables
 urls = ('/', 'index',
-    )
+        '/manualcontroller', 'manualcontroller'
+        )
 
 
 class index:
+
     def GET(self):
         return render.index()
-'''
-#get or post -> function
-class delete:
-    #GET function
-    def GET(self):
-        # url eg. http://192.168.1.3:1234/delete?id=1
-        i = web.input(id=None)
-        #db
-        sdb = sqldb()
-        sdb.cu.execute('delete from msgs where id = ?', i.id)
-        sdb.conn.commit()
-        #go to /viewPost page
-        return web.seeother('/viewPost')
 
-
-class edit:
-    #POST function
-    def POST(self):
-        #the form is i
-        i = web.input()
-        date = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())
-        sdb = sqldb()
-        i = (i.name, i.content, i.id)
-        rec = sdb.cu.execute(
-            'update msgs set name = ?,content= ? where id = ?', i)
-        sdb.conn.commit()
-        return web.seeother('/viewPost')
+def activer(name,value):
+    print name,value
+    return 
+class manualcontroller:
 
     def GET(self):
         return web.seeother('/')
 
-
-class new:
     def POST(self):
         i = web.input()
-        date = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())
-        sdb = sqldb()
-        rec = sdb.cu.execute('select * from msgs')
-        dbre = sdb.cu.fetchall()
-        for k in dbre:
-            j = k[0] + 1
-
-        t = (j, i.name, date, i.content)
-        print t
-        sdb.cu.execute('insert into msgs values(?,?,?,?)', t)
-        sdb.conn.commit()
-        return web.seeother('/viewPost')
-
-    def GET(self):
-        return web.seeother('/')
-
-
-class newPost:
-    def GET(self):
-        return render.newPost()
-
-
-class editPost:
-    def GET(self):
-        i = web.input()
-        sdb = sqldb()
-        rec = sdb.cu.execute('select * from msgs where id = ?', i.id)
-        dbre = sdb.cu.fetchall()
-        return render.editPost(dbre)
-
-
-class viewPost:
-    def GET(self):
-        sdb = sqldb()
-        rec = sdb.cu.execute('select * from msgs')
-        dbre = sdb.cu.fetchall()
-        return render.viewPost(dbre)
-'''
+        print i.switcher1, i.servor1, i.led1
+        if i.switcher1 == 1 or i.switcher1 ==0 :
+            activer("switcher1",i.switcher1)
+            
+        if i.servor1>=0 and i.servor1 <=180:
+            activer("servor1",i.servor1)
+            
+        if i.led1 !="":
+            activer("led1",i.led1)
+        '''
+        db = sqldb()
+        db.cu.execute('')
+        rs = db.cu.fetchall()
+        for r in rs:
+            print r[0]
+        db.conn.commit()
+        '''
+        return render.seeother('/')
 
 class sqldb:
+
     def __init__(self):
-        if os.path.exists('msg.db'):
-            self.conn = sqlite3.connect('msg.db')
+        self.db = 'db.db'
+        if os.path.exists(self.db):
+            self.conn = sqlite3.connect(self.db)
             self.cu = self.conn.cursor()
         else:
-            self.conn = sqlite3.connect('msg.db')
+            self.conn = sqlite3.connect(self.db)
             self.cu = self.conn.cursor()
-            self.cu.execute('create table msgs(id integer primary key,name text,date text,content text)')
-            self.cu.execute("insert into msgs values(1,'gong','2016-05-16 16:36:00','hello gong')")
+            self.cu.execute(
+                'create table msgs(id integer primary key,name text,date text,content text)')
+            self.cu.execute(
+                "insert into msgs values(1,'gong','2016-05-16 16:36:00','hello gong')")
             self.conn.commit()
 
+                
 
 if __name__ == '__main__':
     app = web.application(urls, globals())
