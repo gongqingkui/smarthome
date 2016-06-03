c9f4d253c83d9dd4348e072b43c0bf22e116a844
gongqingkui WinTabletPc <gongqingkui@126.com.winpc>
2016-06-03 10:40:29 +0800

    serial code test

--- serialPortModule.py
+++ serialPortModule.py
@@ -1,5 +1,16 @@
 def serialSend(name,value):
-    #serial port init
-    #serial send name,value
-    print "serialSend ",value," to ",name
+    #print "serialSend ",value," to ",name
+    serialSend("SerialSend",value,"to",name)
     return 
+
+def serialInit():  
+    import serial
+    t = serial.Serial('/dev/ttyACM0',9600)
+    print  t.portstr
+    return t
+
+def serialSend(str):
+    t = serialInit()
+    n = t.write(str)
+    str = t.read(20)
+    return n
